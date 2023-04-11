8390b4656d1f74925cede5ae58ba90c4545f77ce
JosephFloresFT <128737688+JosephFloresFT@users.noreply.github.com>
2023-04-11 12:15:46 -0500

    Functioning Player Movement
    read the title. Will update later

--- Unity Github Test Boi/Assets/Scripts/playerController.cs
+++ Unity Github Test Boi/Assets/Scripts/playerController.cs
@@ -6,37 +6,36 @@ public class playerController : MonoBehaviour
 {
     //variables
     public GameObject player;
-    public GameObject playerCenter;
     Rigidbody playerRB;
     public float moveSpeed;
     public float rotationSpeed;
-    public float jumpForce;
-    float horzAxis;
+    float horizAxis;
     float vertAxis;
+    
     
 
     // Start is called before the first frame update
     void Start()
     {
         playerRB = player.GetComponent<Rigidbody>();
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //tryna make em move
-        horzAxis = Input.GetAxis("Horizontal");
+        horizAxis = Input.GetAxis("Horizontal");
         vertAxis = Input.GetAxis("Vertical");
 
-        player.transform.Translate(playerCenter.transform.forward * vertAxis * moveSpeed * Time.deltaTime);
-        //player.transform.Translate(Vector3.left * horzAxis * moveSpeed * Time.deltaTime);
+        player.transform.Translate(Vector3.forward * moveSpeed * vertAxis * Time.deltaTime);
+        player.transform.Rotate(Vector3.up * rotationSpeed * horizAxis * Time.deltaTime);    
 
-        playerCenter.transform.Rotate(Vector3.up * horzAxis * rotationSpeed * Time.deltaTime);
-        
-        //tryna make em jump
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(player.transform.rotation.x > 90 || player.transform.rotation.x < 0)
         {
-            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
+            Debug.Log("Yes");
+        }    
+        
     }
 }
